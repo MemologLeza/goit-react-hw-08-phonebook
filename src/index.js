699c4140bfb9d2b6636f3dee46687de68415064c
MemologLeza <lizabakay1@gmail.com>
2023-09-16 20:38:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App.jsx';
 import 'normalize.css';
 import './index.css';
 import { Provider } from 'react-redux';
-import {store } from 'store/store.js';
+import {store } from './store/store.js';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
